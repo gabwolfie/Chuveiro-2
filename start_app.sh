83d7dcb7e652cc43b78b9228f2fdc177c4d9bb39
gabwolfie <gabrielsk23@gmail.com>
2025-06-28 03:30:17 -0300

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-gunicorn --workers 4 --bind 127.0.0.1:5000 src.main:app
+gunicorn --workers 4 --bind 0.0.0.0:10000 src.main:app
+
