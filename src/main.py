fb2f3d00eddc0a56f13735daefe3965df93013a1
gabwolfie <gabrielsk23@gmail.com>
2025-06-30 19:12:07 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -54,5 +54,8 @@ def handle_join_notifications():
     print('Cliente se inscreveu para notificações')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5003, debug=True)
+    import os
+port = int(os.environ.get("PORT", 5003))
+socketio.run(app, host='0.0.0.0', port=port, use_reloader=False)
+
 
